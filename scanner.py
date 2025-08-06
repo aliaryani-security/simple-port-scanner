144d5e369515e1abd0c90d1426f7532a85a4eee2
Ali Aryani <lenesis@proton.me>
2025-08-06 10:46:21 +0330

    get target

--- scanner.py
+++ scanner.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 
+import socket
+import time
+
+start_time = time.time()
+
+target = input ("specify the host to scan: ")
+target_ip = socket.gethostbyname(target)
+print ("initializing scan for : {} ({})".format(target, target_ip))
