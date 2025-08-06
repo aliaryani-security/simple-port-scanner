19dc275a0ef894923ec4229a3e13f75e37d1ff9e
Ali Aryani <lenesis@proton.me>
2025-08-06 10:55:27 +0330

    scanner loop

--- scanner.py
+++ scanner.py
@@ -8,3 +8,10 @@ start_time = time.time()
 target = input ("specify the host to scan: ")
 target_ip = socket.gethostbyname(target)
 print ("initializing scan for : {} ({})".format(target, target_ip))
+
+for i in range (1,1000):
+    scanner = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    conn = scanner.connect_ex ((target_ip, i))
+    if conn == 0:
+        print (f"[+] Port {i} open")
+    scanner.close()
